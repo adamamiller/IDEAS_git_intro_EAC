aed9af585ca7dda425dcd0bfa32ff7bd53171596
Adam Miller <amiller.astro@gmail.com>
2019-05-14 13:26:37 -0500

    ENH: lines are better than squiggle

--- python_plot.py
+++ python_plot.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x_arr = np.linspace(0, 6*np.pi)
-
+y = x_arr.copy()
 # Plot a cosine curve
 plt.figure()
-plt.plot(x_arr, np.cos(x_arr), '-.', color='k')
+plt.plot(x_arr, y, '-.', color='k')
 plt.show()
 
 
