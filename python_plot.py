c0b77d60d4626a19a2879cfa534cae06664aa13e
Eve Chase <eve.chase@ligo.org>
2019-05-14 13:33:14 -0500

    ENH: cosine -> sine

--- python_plot.py
+++ python_plot.py
@@ -3,9 +3,9 @@ import numpy as np
 
 x_arr = np.linspace(0, 6*np.pi)
 
-# Plot a cosine curve
+# Plot a sine curve
 plt.figure()
-plt.plot(x_arr, np.cos(x_arr), '-.', color='k')
+plt.plot(x_arr, np.sin(x_arr), '-.', color='k')
 plt.show()
 
 
